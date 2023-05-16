a9543f34c70f154065ebce7102c0d3f678dc26f2
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-16 17:26:26 +0200

    lombok fixed

--- src/main/java/entities/Evento.java
+++ src/main/java/entities/Evento.java
@@ -10,8 +10,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "eventi")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Evento {
 	@Id
 	@GeneratedValue
@@ -23,9 +32,6 @@ public class Evento {
 	private TipoEvento tipoEvento;
 	private int numeroMassimoPartecipanti;
 
-	public Evento() {
-	}
-
 	public Evento(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento,
 			int numeroMassimoPartecipanti) {
 		super();
@@ -34,60 +40,6 @@ public class Evento {
 		this.descrizione = descrizione;
 		this.tipoEvento = tipoEvento;
 		this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getTitolo() {
-		return titolo;
-	}
-
-	public void setTitolo(String titolo) {
-		this.titolo = titolo;
-	}
-
-	public LocalDate getDataEvento() {
-		return dataEvento;
-	}
-
-	public void setDataEvento(LocalDate dataEvento) {
-		this.dataEvento = dataEvento;
-	}
-
-	public String getDescrizione() {
-		return descrizione;
-	}
-
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-
-	public TipoEvento getTipoEvento() {
-		return tipoEvento;
-	}
-
-	public void setTipoEvento(TipoEvento tipoEvento) {
-		this.tipoEvento = tipoEvento;
-	}
-
-	public int getNumeroMassimoPartecipanti() {
-		return numeroMassimoPartecipanti;
-	}
-
-	public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
-		this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
-	}
-
-	@Override
-	public String toString() {
-		return "Id: " + id + ", Titolo: " + titolo + ", Data evento: " + dataEvento + ", Descrizione: " + descrizione
-				+ ", Tipo evento: " + tipoEvento + ", Numero massimo partecipanti: " + numeroMassimoPartecipanti;
 	}
 
 }
